6e41ffcc6b4b09a1b3dca4e0858f81d9bcfd4833
Boy0211 <s.kuilboer@gmail.com>
2018-11-13 13:44:56 +0100

    score berekenen

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ sys.path.append(os.path.join(directory, "code", "classes"))
 
 from greedy import greedy_1
 from smartGRID import Smartgrid
+from score import calculate_score
 
 
 def main():
-    smartgrid = Smartgrid(1)
+    smartgrid = Smartgrid(3)
     greedy_1(smartgrid.houses, smartgrid.batterys)
     for battery in smartgrid.batterys:
         print(battery)
+    score = calculate_score(smartgrid.houses)
+    print(score)
+
 
 
 if __name__ == "__main__":
