a07f374885b43bba0f197e3b557411cd981df7d9
Mendel Engelaer <mendelengelaer@wcw-stud-145-109-9-115.wireless.uva.nl>
2018-11-13 11:31:59 +0100

    tree cleanup

--- code/algorithmes/greedy.py
+++ code/algorithmes/greedy.py
@@ -5,19 +5,24 @@ class Greedy():
     def greedy_1(self):
 
         house_counter = 0
+
+        # iterate through all houses
         while house_counter < len(self.houses):
             bat_list = []
+
+            # append all batteries in new list and choose battery with lowest
+            # current input
             for battery in self.batterys:
                 bat_list.append(battery.current_input)
             battery_counter = bat_list.index(min(bat_list))
-            # print(battery_counter)
+
             del(bat_list)
 
+            # if a house fits into the battery with the lowest input, put this
+            # house inside this battery. Otherwise were scruwed because we have
+            # reached the max capacity of all batteries
             if self.batterys[battery_counter].current_input + self.houses[house_counter].output < 1507:
                 self.add_house_to_battery(self.houses[house_counter], self.batterys[battery_counter])
             else:
                 print("het past gewoon godverdomme niet!")
             house_counter += 1
-
-        # for house in self.houses:
-            # print(house)
