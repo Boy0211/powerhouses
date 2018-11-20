fb3498b72c04bc938ab5e52affe9c81066192381
Mendel Engelaer <mendelengelaer@wcw-stud-145-109-15-178.wireless.uva.nl>
2018-11-20 09:50:13 +0100

    greedy opgeschoont

--- code/algorithmes/greedy.py
+++ code/algorithmes/greedy.py
@@ -33,64 +33,34 @@ def greedy_2(houses, batterys):
     ''' Greedy algorithm that fills up the most empty battery on a sorted list'''
 
     houses = sort_distance(houses)
-    # for house in houses:
-    #     print(house)
-    # for battery in batterys:
-    #     print(battery)
-    # iterate through all houses
 
+    # iterate through all houses
     house_counter = 0
     temp_house_counter = 0
     temp_dict = dict()
-    # for house in houses:
-        # print(house.battery_distances)
-    # try_battery = 0
+
     while house_counter < len(houses):
 
         # als house_counter toeneemt, sla battery_distances op in nieuwe lijst
         if temp_house_counter == house_counter:
-            # print(houses[house_counter].battery_distances)
             temp_dict = dict()
-            # print(temp_dict)
             temp_dict = copy.deepcopy(houses[house_counter].battery_distances)
-            # print(temp_dict)
-            # print(houses[house_counter].battery_distances)
-
 
         # sorteer lijst van batterijen per huis
         battery_list = (list(temp_dict.values()))
         battery_list = sorted(battery_list)
-        # print(houses[house_counter].battery_distances)
 
         # neem eerste waarde in deze lijst(is minimale waarde)
-        # print(f"try battery: {try_battery}")
-        # print(battery_list)
         current_battery = (battery_list[0])
 
         # zoek key met bijbehorende afstand
         battery_number = (list(temp_dict.keys())[list(temp_dict.values()).index(current_battery)])
-        # print(f"battery number: {battery_number}")
-        # print(f"try battery: {try_battery}")
 
         if batterys[battery_number-1].current_input + houses[house_counter].output <= float(batterys[battery_number-1].max_input):
             add_house_to_battery(houses[house_counter], batterys[battery_number-1])
-            # print(houses[house_counter].battery_distances)
-
 
             house_counter += 1
             temp_house_counter = house_counter
-            # try_battery = 0
-            # for battery in batterys:
-                # print(battery)
         else:
-            # print("niet gelukt")
-            # print(try_battery)
-            # print(houses[house_counter].output)
             temp_house_counter += 1
             del temp_dict[battery_number]
-
-
-    # for house in houses:
-    #     print(house)
-    # for battery in batterys:
-    #     print(battery)
