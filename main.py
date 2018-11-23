8ab4ec7ceb9aa4b2b0758007e5d59e76893c4e0e
Rutger Storm <rutger.storm97@gmail.com>
2018-11-23 11:29:29 +0100

    Grid functie in de main

--- main.py
+++ main.py
@@ -19,29 +19,6 @@ from hillclimber import hill_climber_2
 
 def main():
 
-<<<<<<< HEAD
-    smartgrid = Smartgrid(3)
-    greedy_1(smartgrid.houses, smartgrid.batterys)
-    grid(smartgrid.houses, smartgrid.batterys)
-    # i = 0
-    # all_scores = []
-    # time_stamp = datetime.now()
-    # while i < 100:
-    #     random_greedy(smartgrid.houses, smartgrid.batterys)
-    #     score = calculate_score(smartgrid.houses)
-    #     all_scores.append(score)
-    #     i += 1
-    #
-    # print(datetime.now() - time_stamp)
-    # all_scores.sort()
-    # bins = 25
-    # n,x,_ = plt.hist(all_scores, bins)
-    # bin_centers = 0.5*(x[1:]+x[:-1])
-    # plt.plot(bin_centers, n)
-    # plt.show()
-    # for battery in smartgrid.batterys:
-    #     print(battery)
-=======
     smartgrids = []
     for i in range(5):
         smartgrid = Smartgrid(1)
@@ -53,6 +30,7 @@ def main():
     # greedy gebasseerd op capaciteit gevolgd door een random hill_climber
     greedy_1(smartgrids[0].houses, smartgrids[0].batterys)
     hill_climber(smartgrids[0].houses, smartgrids[0].batterys)
+    grid(smartgrids[0].houses, smartgrids[0].batterys)
     print(calculate_score(smartgrids[0].houses))
     print("-------------------------------------")
 
@@ -61,6 +39,7 @@ def main():
     # gebasseerd op capaciteit gevolgd door een best choice hill hill_climber
     greedy_1(smartgrids[1].houses, smartgrids[1].batterys)
     hill_climber_2(smartgrids[1].houses, smartgrids[1].batterys)
+    grid(smartgrids[1].houses, smartgrids[1].batterys)
     print(calculate_score(smartgrids[1].houses))
     print("-------------------------------------")
 
@@ -69,6 +48,7 @@ def main():
     # gebasseerd op een distance greedy gevolg door random hill_climber
     greedy_2(smartgrids[2].houses, smartgrids[2].batterys)
     hill_climber(smartgrids[2].houses, smartgrids[2].batterys)
+    grid(smartgrids[2].houses, smartgrids[2].batterys)
     print(calculate_score(smartgrids[2].houses))
     for battery in smartgrids[2].batterys:
         print(battery)
@@ -79,6 +59,7 @@ def main():
     # gebaseerd op een distance greedy gevolgd door een best choice hill climber
     greedy_2(smartgrids[3].houses, smartgrids[3].batterys)
     hill_climber_2(smartgrids[3].houses, smartgrids[3].batterys)
+    grid(smartgrids[3].houses, smartgrids[3].batterys)
     print(calculate_score(smartgrids[3].houses))
     for battery in smartgrids[2].batterys:
         print(battery)
@@ -90,7 +71,6 @@ def main():
     # random_distribution(smartgrids[4].houses, smartgrids[4].batterys, 100, 15)
     # print("-------------------------------------")
 
->>>>>>> 371470cb15d26cd06db9d1efb43a2617f1c66886
     exit()
 
 
